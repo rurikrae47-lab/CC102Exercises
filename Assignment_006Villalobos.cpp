9fd659fb0e2263ea73c51b4a23660378565855de
rurikrae47-lab <rurikrae47@gmail.com>
2026-04-28 13:08:04 +0800

    Create Assignment_006Villalobos.cpp

---
+++ Assignment_006Villalobos.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    for (int i = 3; i * i <= n; i += 2)
+        if (n % i == 0) return false;
+    return true;
+}
+
+int countPrimesInFile(const string& filename) {
+    ifstream file(filename);
+    int num, count = 0;
+    while (file >> num) {
+        if (isPrime(num)) count++;
+    }
+    file.close();
+    return count;
+}
+
+int main() {
+    cout << countPrimesInFile("NUMS.TXT");
+    return 0;
+}
